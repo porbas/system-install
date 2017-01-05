d6ea9ed8e28a34ace20cf9866a44e7a4a5c05292
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2017-01-05 10:11:42 +0100

    ansible-setup chmod

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -2,6 +2,7 @@
 
 apt-get update
 apt-get install --no-install-recommends rsync git python-jinja2 python-yaml
-git clone --recurse-submodules https://github.com/ansible/ansible.git ~/Developer/ansible -b stable-2.2
-echo "run following command to get working ansible:\n . ansible/hacking/env-setup"
+git clone --recurse-submodules https://github.com/ansible/ansible.git ansible -b stable-2.2
+echo "run following command to get working ansible:"
+echo " . ansible/hacking/env-setup"
 
